98967e8ed5e692120574fe524225c8df54429af2
Minh Triet <triettran332003@gmail.com>
2024-10-18 10:45:37 +0700

    Simplify training model

--- actions/actions.py
+++ actions/actions.py
@@ -1,37 +1,7 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
-from typing import Any, Coroutine, Text, Dict, List
+from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet, EventType
-import webbrowser
-from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
 
 class ActionExtractSymptom(Action):
     def name(self) -> Text:
@@ -40,7 +10,8 @@ class ActionExtractSymptom(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         entities = tracker.latest_message.get('entities', [])
         symptom = None
-        print('entities: ', entities)
+
+        # Extract the symptom entity from the latest message
         if entities:
             for entity in entities:
                 if entity['entity'] == 'symptom':
@@ -48,10 +19,11 @@ class ActionExtractSymptom(Action):
         
         if symptom:
             response = f"I see that you mentioned {symptom} as a symptom."
+            # Save the symptom to a slot
+            return [SlotSet("symptom", symptom)]
         else:
             response = "Could you please specify the symptom you are experiencing?"
 
         dispatcher.utter_message(text=response)
 
         return []
-   
